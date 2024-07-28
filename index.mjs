9f0dd6e780528027f8aa4db5dd5364943885ae31
Grigoriy Levanov <gregory@levanov.io>
2024-07-28 12:36:33 +0200

    fix get requests

--- index.mjs
+++ index.mjs
@@ -7,25 +7,23 @@ import { addWeight } from "./src/add-weight.mjs";
 const server = createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", config.frontendUrl);
 
-  let body = "";
-  if (req.method === "POST") {
+  if (req.url === "/weights" && req.method === "GET") {
+    getWeights((result) => {
+      if (result.isSuccess) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(result.data));
+      } else {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end(result.error.toString());
+      }
+    });
+  } else if (req.url === "/weights" && req.method === "POST") {
+    let body = "";
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
-  }
 
-  req.on("end", () => {
-    if (req.url === "/weights" && req.method === "GET") {
-      getWeights((result) => {
-        if (result.isSuccess) {
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(result.data));
-        } else {
-          res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end(result.error.toString());
-        }
-      });
-    } else if (req.url === "/weights" && req.method === "POST") {
+    req.on("end", () => {
       addWeight(body, (result) => {
         if (result.isSuccess) {
           res.writeHead(201, { "Content-Type": "text/plain" });
@@ -35,11 +33,11 @@ const server = createServer((req, res) => {
           res.end(result.error.toString());
         }
       });
-    } else {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Not found");
-    }
-  });
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not found");
+  }
 });
 
 server.listen(config.port, () => {
